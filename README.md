a96101a3cf913f699b6f6598cf7514236b41d2fb
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2024-10-06 11:41:07 -0300

    Aula 01

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ecommerce-nextjs
+# Projeto E-commerce - Formação Dev
+
+Curso realizado gratuitamente através da plataforma Escola Formação Dev (Cod3r) na Trilha Profissional de Mercado.
+
+Tecnologias Utilizadas:
+- Next.js
+- Typescript
+- React.js
+- Tailwind.css
+- Tabler Icons
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import './globals.css'
+import { Montserrat } from 'next/font/google'
+
+const font = Montserrat({
+    subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+    title: 'Gamer Store',
+    description: 'Loja Gamer Store',
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="pt-br">
+            <body className={font.className}>{children}</body>
+        </html>
+    )
+}
